19c9fc42af4f80c1640045a3f4d1c770682af162
CHC <howjerry22@gmail.com>
2021-05-10 13:43:56 +0800

    建立

---
+++ NotifyType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CHC.ToastrNotify
+{
+    public enum NotifyType
+    {
+        Success,
+        Info,
+        Error,
+        Warning
+    }
+}
